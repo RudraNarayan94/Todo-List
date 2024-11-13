a2ee0dae5610bc3a7fd5ce4f49de83536b7c42cb
Rudra <rudranarayans10@gmail.com>
2024-11-13 18:25:08 +0530

    Adding componets

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { TodoProvider } from "./contexts";
-import { useEffect } from "react";
+import { TodoFrom, TodoItem } from "./components";
 function App() {
   const [todos, setTodos] = useState([]);
 
@@ -48,9 +48,17 @@ function App() {
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
             Manage Your Todos
           </h1>
-          <div className="mb-4">{/* Todo form goes here */}</div>
+          <div className="mb-4">
+            {/* Todo form goes here */}
+            <TodoFrom />
+          </div>
           <div className="flex flex-wrap gap-y-3">
             {/*Loop and Add TodoItem here */}
+            {todos.map((todo) => (
+              <div key={todo.id} className="w-full">
+                <TodoItem todo={todo} />
+              </div>
+            ))}
           </div>
         </div>
       </div>
